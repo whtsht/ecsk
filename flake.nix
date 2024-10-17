b28dbe75f0972fa7cbceb75fb1b035c113762980
whtsht <whiteshirt0079@gmail.com>
2024-10-17 23:52:06 +0900

    fix unpack command

--- flake.nix
+++ flake.nix
@@ -4,34 +4,28 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, ... }: {
-    packages = {
-      x86_64-linux.ecsk = buildEcs "x86_64-linux";
-      aarch64-linux.ecsk = buildEcs "aarch64-linux";
-    };
-  };
+    packages.x86_64-linux.ecsk = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      version = "0.9.3";
+      src = pkgs.fetchurl {
+        url = "https://github.com/yukiarrr/ecsk/releases/download/v${version}/ecsk_Linux_x86_64.tar.gz";
+        sha256 = "0gl12pw8dqrc9p7qnxqnzzdd7grn0p1sv6xps62pbi5yrrasjpag";
+      };
+    in
+    pkgs.stdenv.mkDerivation {
+      pname = "ecsk";
+      inherit version src;
 
-  buildEcs = system: let
-    pkgs = nixpkgs.legacyPackages.${system};
+      unpackPhase = ''
+        mkdir -p $out/bin
+        tar -xzf $src -C $out/bin
+        chmod +x $out/bin/ecsk
+      '';
 
-    version = "0.9.3";
-    src = pkgs.fetchurl {
-      url = "https://github.com/yukiarrr/ecsk/releases/download/v${version}/ecsk_Linux_${system}.tar.gz";
-      sha256 = "0gl12pw8dqrc9p7qnxqnzzdd7grn0p1sv6xps62pbi5yrrasjpag";
-    };
-  in
-  pkgs.stdenv.mkDerivation {
-    pname = "ecsk";
-    inherit version src;
-
-    unpackPhase = ''
-      mkdir -p $out/bin
-      tar -xzf ${src} -C $out/bin --strip-components=1
-      chmod +x $out/bin/ecsk
-    '';
-
-    meta = with pkgs.lib; {
-      description = "ECS + Task = ecsk 😆";
-      license = licenses.mit;
+      meta = with pkgs.lib; {
+        description = "ECS + Task = ecsk 😆";
+        license = licenses.mit;
+      };
     };
   };
 }
